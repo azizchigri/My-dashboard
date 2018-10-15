480115a25749fe582235393a1de936bffd6398eb
azizc <aziz.chigri@epitech.eu>
2018-10-15 14:50:06 +0200

    [Spring Security] Authorize front html/css/js on server

--- server/src/main/java/com/bbsn/application/security/WebSecurity.java
+++ server/src/main/java/com/bbsn/application/security/WebSecurity.java
@@ -47,6 +47,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.cors().and().csrf().disable().authorizeRequests().antMatchers(HttpMethod.POST, SIGN_UP_URL).permitAll()
+				.antMatchers("/resources/**").permitAll().anyRequest().permitAll()
 				.antMatchers(HttpMethod.GET, "/about.json").permitAll().anyRequest().authenticated().and()
 				.addFilter(new JWTAuthenticationFilter(authenticationManager()))
 				.addFilter(new JWTAuthorizationFilter(authenticationManager())).sessionManagement()
