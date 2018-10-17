57f4d6a2876e77ee893284af988418d9f78686d5
cbenjamin <40006497+bchadelaud@users.noreply.github.com>
2018-10-17 17:06:17 +0200

    [ADD] token cookie + working on dashboad presentation

--- front/assets/js/login.js
+++ front/assets/js/login.js
@@ -1,50 +1,27 @@
-function setCookie(cname, cvalue, exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    var expires = "expires="+ d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
-
-function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for(var i = 0; i <ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
-}
-
 function    loginMe()
 {
-	console.log(JSON.stringify({ username : $('#loginUsername').val(), password : $('#loginPassword').val() }));
     $.ajax({
         url: '/server/login',
         type: 'POST',
         data: JSON.stringify({ username : $('#loginUsername').val(), password : $('#loginPassword').val() }),
         contentType: "application/json",
-        complete: function(result) {
-            console.log(result);
-            var error = '<div class="alert alert-danger alert-dismissible fade in " >'
-            error += '<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>'
-            error += '<strong>Error</strong>'
-            error += '</div>'
-            $("#loginDisplay").append(error)
+        complete: function(result, status) {
+            if (status == 'success') {
+                console.log("connecté");
+                setCookie("username", $('#loginUsername').val(), 1)
+                setCookie("authorization", result.getResponseHeader("authorization"), 1)
+                $("#loginModal").modal("hide");
+            } else
+            {
+                console.log("pas connecté")
+                var error = '<div class="alert alert-danger alert-dismissible fade in " >'
+                error += '<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>'
+                error += '<strong>Error</strong>'
+                error += '</div>'
+                $("#loginDisplay").append(error)
+            }
+            console.log(getCookie("username"))
+            console.log(getCookie("authorization"))
         }
     });
-    if (data) {
-        var user = JSON.parse(data);
-        console.log("connecté")
-    } else
-    {
-        console.log("pas connecté")
-    }
-    console.log("unsername:" + user.username)
-    console.log("clicked")
 }
--- front/assets/js/register.js
+++ front/assets/js/register.js
@@ -1,17 +1,25 @@
 function    registerMe()
 {
 	$.ajax({
-    url: 'server/sign-up',
+    url: '/server/sign-up',
     type: 'POST',
     data: JSON.stringify({ email: $('#registerEmail').val(), username : $('#registerUsername').val(), firstName : $('#registerFirstname').val(), lastName : $('#registerLastname').val(), password : $('#registerPassword').val() }),
     contentType: "application/json",
-    complete: function(result) {
+    complete: function(result, status) {
         console.log(result);
-        var error = '<div class="alert alert-danger alert-dismissible fade in " >'
-        error += '<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>'
-        error += '<strong>Error</strong>'
-        error += '</div>'
-        $("#loginDisplay").append(error)
+        if (status == 'success') {
+            $("#registerModal").modal("hide");
+        } else {
+            respond = JSON.parse(result.responseText);
+            var error = '<div class="alert alert-danger alert-dismissible fade in text-left" >'
+            error += '<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a> <strong>'
+            for (err in respond.errors) {
+                console.log(respond.errors[err].defaultMessage);
+                error += respond.errors[err].defaultMessage + '<br>';
+            }
+            error += '</strong></div>'
+            $("#registerDisplay").append(error)
+        }
     }
 });
     console.log("clicked")
