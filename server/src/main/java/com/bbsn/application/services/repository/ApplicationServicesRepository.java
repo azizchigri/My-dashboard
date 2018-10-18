42caea990e22534b5c824acabfdc4014ff705494
Aziz Chigri <aziz.chigri@epitech.eu>
2018-10-18 20:31:34 +0200

    [Fix] There was errors on widgets/services (/widget instead of /services)

--- server/src/main/java/com/bbsn/application/services/repository/ApplicationServicesRepository.java
+++ server/src/main/java/com/bbsn/application/services/repository/ApplicationServicesRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.bbsn.application.services.model.Services;
 
 public interface ApplicationServicesRepository extends JpaRepository<Services, String> {
+	Services findByName(String serviceName);
 }
-
